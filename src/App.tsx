f205cd7c628832fd54685a02e04f1a1552727369
Victor Nomwesigwa <bethel@Bethel.local>
2019-11-13 10:21:26 +0300

    ch- Set up the frontend - create a react app with typescript
    [Starts #169723194]

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          Welcome to the My Arcade Application
+        </p>
+        
+      </header>
+    </div>
+  );
+}
+
+export default App;
