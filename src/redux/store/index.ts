7671f89a3c5488b799cdd52f25abbcad946ec799
Charles Kato <kcharles52@gmail.com>
2019-12-11 11:18:25 +0300

    chore(testing): setup unit test environment
    - install jest and enzyme
    - write configurations for jest and babel
    
    [MANTAINS #169723191]

--- src/redux/store/index.ts
+++ src/redux/store/index.ts
@@ -1,11 +1,11 @@
 import {applyMiddleware, combineReducers, createStore} from "redux";
 import thunk from 'redux-thunk';
-
-const createReducer = (initialState: any, handlers: { [x: string]: (arg0: any, arg1: any) => any; }) => {
-    return (state = initialState, action: { type: string | number; }) => {
-        return (handlers[action.type] && handlers[action.type](state, action)) || state;
-    }
-};
+//TODO: To be un-commented when needed.
+// const createReducer = (initialState: any, handlers: { [x: string]: (arg0: any, arg1: any) => any; }) => {
+//     return (state = initialState, action: { type: string | number; }) => {
+//         return (handlers[action.type] && handlers[action.type](state, action)) || state;
+//     }
+// };
 
 const rootReducer = combineReducers({});
 
